e2fd68cc4454e54111855543b99268d3e5d1b5d9
den <kickrop@gmail.com>
2019-02-10 09:39:06 +0300

    named path

--- src/components/user/UserDetail.vue
+++ src/components/user/UserDetail.vue
@@ -2,7 +2,10 @@
     <div>
         <h3>Some User Details</h3>
         <p>User loaded has ID: {{ $route.params.id }}</p>
-        <router-link tag="button" :to="'/user/' + $route.params.id + '/edit'"
+        <router-link 
+        tag="button" 
+        :to="{ name: 'userEdit', params: { id: $route.params.id } }"
         class="btn btn-primary">Edit user</router-link>
     </div>
 </template>
+<!-- :to="'/user/' + $route.params.id + '/edit'" manual way-->
