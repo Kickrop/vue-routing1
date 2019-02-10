14340d74aad8f4ec49e7d5232ecb83ac822dfcb4
den <kickrop@gmail.com>
2019-02-10 09:26:55 +0300

    nested routes

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -4,18 +4,25 @@
         <hr>
         <button @click="navigateToHome" class="btn btn-primary">Go home</button>
         <hr>
+        <router-view></router-view>
+        <!-- <hr>
         <br>
-        <p>Loaded ID: {{ id }}</p>
+        <p>Loaded ID: {{ id }}</p> -->
     </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            id: this.$route.params.id
-        }
-    },
+    // data() {
+    //     return {
+    //         id: this.$route.params.id
+    //     }
+    // },
+    // watch: {
+    //     '$route'(to, from) {
+    //         this.id = to.params.id;
+    //     }
+    // }, /*in a new version their is no need for watch https://github.com/vuejs/vue-router/tree/dev/examples/route-props*/ */
     methods: {
         navigateToHome() {
             this.$router.push('/');
