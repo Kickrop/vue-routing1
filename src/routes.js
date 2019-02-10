71cc9edffab6bf6bab66c4b21cce0ea3b186785a
den <kickrop@gmail.com>
2019-02-10 10:01:02 +0300

    catch all route

--- src/routes.js
+++ src/routes.js
@@ -19,6 +19,7 @@ export const routes = [
         { path: ':id', component: UserDetail },
         { path: ':id/edit', component: UserEdit, name: 'userEdit' }
     ] },
-    { path: '/redirect-me', redirect: '/user' } //redirect: { name: 'home'}
+    { path: '/redirect-me', redirect: '/user' }, //redirect: { name: 'home'}
+    { path: '*', redirect: '/' } //everything except paths above will lead to home
 ];
 // routes can be named. name: home
